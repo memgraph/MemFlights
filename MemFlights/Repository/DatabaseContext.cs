53880b339caa4185d4c7a85cd176c47a0d1563aa
nmotocic <48250159+nmotocic@users.noreply.github.com>
2021-02-19 16:37:36 +0100

    initial commit

---
+++ MemFlights/Repository/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+
+namespace MemFlights.Repository
+{
+    public class DatabaseContext
+    {
+        public static IDriver dbDriver;
+
+        public static void Register() {
+            dbDriver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.None);
+        } 
+    }
+}
